43f30709bfa1bf26c588c655fa703fa9411cb0a5
DShestavin <114399365+DShestavin@users.noreply.github.com>
2023-12-16 15:44:10 +0300

    Create 2 Even Fibonacci Numbers.cs

---
+++ 2 Even Fibonacci Numbers.cs
@@ -0,0 +1,15 @@
+﻿int numberFibonachi1 = 1;
+int numberFibonachi2 = 2;
+int numberFibonachi3 = 0;
+int sumPositivNumbers = 2;
+
+while(numberFibonachi3 < 4000000)
+{
+    numberFibonachi3 = numberFibonachi1 + numberFibonachi2;
+    numberFibonachi1 = numberFibonachi2;
+    numberFibonachi2 = numberFibonachi3;
+    if (numberFibonachi3 % 2 == 0)
+        sumPositivNumbers += numberFibonachi3;
+}
+
+Console.WriteLine(sumPositivNumbers);
