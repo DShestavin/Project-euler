259bcfafd5933b96935c6eea8222ec2895e13400
DShestavin <114399365+DShestavin@users.noreply.github.com>
2023-12-17 16:56:48 +0300

    Create 6 Sum Square Difference.cs

---
+++ 6 Sum Square Difference.cs
@@ -0,0 +1,11 @@
+﻿int numberSquaresSun = 0;
+int numberSumSquares = 0;
+
+for (int i = 1; i < 101; i++) // находим сумму квадратов
+    numberSquaresSun += (i * i);
+
+
+for (int i = 1; i < 101; i++) // находим сумму чмсел от 1 до 100
+    numberSumSquares += i;
+
+Console.WriteLine(numberSumSquares * numberSumSquares - numberSquaresSun);
