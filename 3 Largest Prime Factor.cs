44646411ded89bad49aa6ebcd84049c24cd2b242
DShestavin <114399365+DShestavin@users.noreply.github.com>
2023-12-17 12:05:45 +0300

    Create 3 Largest Prime Factor.cs

---
+++ 3 Largest Prime Factor.cs
@@ -0,0 +1,23 @@
+﻿long number = 600851475143;
+int total = 2;
+int maxTotal = 0;
+
+while (number != 1)
+{
+    while (total < number)
+    {
+        total += 1;
+        if (number % total == 0)
+        {
+            number /= total;
+            if (total > maxTotal)
+            {
+                maxTotal = total;
+            }
+            break;
+        }    
+    }
+    total = 2;
+}
+
+Console.WriteLine(maxTotal);
