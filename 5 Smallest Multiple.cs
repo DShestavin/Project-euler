31d7497d70649f4e49d3f1d63fb34de3b1025fd6
DShestavin <114399365+DShestavin@users.noreply.github.com>
2023-12-17 13:24:18 +0300

    Create 5 Smallest Multiple.cs

---
+++ 5 Smallest Multiple.cs
@@ -0,0 +1,19 @@
+﻿long number = 21;
+int total = 0; // счетчик количества делителей от 1 до 20
+
+while (total != 20)
+{
+    for (int i = 1; i < 21; i++)
+    {
+        if (number % i == 0)
+        {
+            total++;
+        }
+    }
+    if (total == 20)
+        break;
+    total = 0;
+    number++;
+}
+
+Console.WriteLine(number);
