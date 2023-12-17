0d1e74af069ae176ef47b898e05c72af2c19a584
DShestavin <114399365+DShestavin@users.noreply.github.com>
2023-12-17 17:08:36 +0300

    Create 7 10001 st Prime.cs

---
+++ 7 10001 st Prime.cs
@@ -0,0 +1,19 @@
+﻿int numbers = 2;
+int totalNumber = 1; // номер простого числа
+
+while (totalNumber != 10001)
+{
+    int total = 0;
+    numbers++;
+    for (int i = 2; i <= numbers; i++) // проверка простое число или нет
+    {
+        if (numbers % i == 0)
+            total++;
+        if (total == 2) 
+            break;
+    }
+    if (total == 1)
+        totalNumber++;
+}
+
+Console.WriteLine(numbers);
