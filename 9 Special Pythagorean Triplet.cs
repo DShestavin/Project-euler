3fe7b30fae7a6d6c3a18a86d0592ce6eed23e485
DShestavin <114399365+DShestavin@users.noreply.github.com>
2023-12-17 17:38:10 +0300

    Create 9 Special Pythagorean Triplet.cs

---
+++ 9 Special Pythagorean Triplet.cs
@@ -0,0 +1,5 @@
+﻿for (int a = 1; a < 1000; a++)
+    for (int b = 0; b < 1000; b++)
+        for (int c = 0; c < 1000;  c++)
+            if (((a * a) + (b * b) == (c * c)) && ((a + b + c == 1000)))
+                Console.WriteLine(a * b * c);
