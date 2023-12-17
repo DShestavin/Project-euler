b80c1c6fb1f32074a54f66580ea4b3dbd320afd9
DShestavin <114399365+DShestavin@users.noreply.github.com>
2023-12-17 13:27:31 +0300

    Create 4 Largest Palindrome Product.cs

---
+++ 4 Largest Palindrome Product.cs
@@ -0,0 +1,22 @@
+﻿int value1 = 999;
+int value2 = 999;
+int maxPalindom = 0;
+
+while (value1 > 99)
+{
+    while (value2 > 99)
+    {
+        string palindom1 = (value1 * value2 / 1000).ToString();
+        string palindom2 = (value2 * value1 % 10).ToString() + (value2 * value1 % 100 / 10).ToString() + (value2 * value1 % 1000 / 100).ToString();
+        if (palindom1 == palindom2)
+        {
+            if ((value1 * value2) > maxPalindom)
+                maxPalindom = value1 * value2;
+        }
+        value2--;
+    }
+    value2 = 999;
+    value1--;
+}
+
+Console.WriteLine(maxPalindom);
