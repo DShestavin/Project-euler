ba96b8ba69062fa6eb5f8bbcf52e1ac8888e8fb8
DShestavin <114399365+DShestavin@users.noreply.github.com>
2023-12-26 15:21:35 +0300

    Create 12 Highly Divisible Triangular Number.cs

---
+++ 12 Highly Divisible Triangular Number.cs
@@ -0,0 +1,23 @@
+﻿int number = 1;
+int numb = 0;
+int devisorCount = 0;
+
+while (devisorCount < 501)
+{
+    number++;
+    devisorCount = 0;
+    numb = 0;
+    for (int i = 1; i <= number; i++) //создание треугольного числа
+    {
+        numb += i;
+    }
+    for (int i = 1; i <= numb; i++) //поиск делителей
+    {
+        if (numb % i == 0)
+        {
+            devisorCount++;
+        }
+    }
+}
+
+Console.WriteLine(numb);
